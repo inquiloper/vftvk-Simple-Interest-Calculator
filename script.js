90c73607224f9ad5a1842bc5fe1bdbe98477989b
Inquiloper <inquiloper@gmail.com>
2022-02-07 02:14:07 +0000

    added enhancements

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ function compute()
     var year = new Date().getFullYear()+ parseInt(years);
 
     var text = `
-    If you deposit ${principal}, <br>
-    at an interest rate of ${rate}%, <br>
-    You will receive an amount of ${interest}, <br>
-    in the year ${year}`;
+    If you deposit <mark>${principal}</mark>, <br>
+    at an interest rate of <mark>${rate}%</mark>, <br>
+    You will receive an amount of <mark>${interest}</mark>, <br>
+    in the year <mark>${year}</mark>`;
 
     document.getElementById("result").innerHTML = text;
 }
